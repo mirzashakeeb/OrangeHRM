866d3eb2bd84975f3e61f339c57234eca690e0d0
mirzashakeeb <shakeeb717171@gmail.com>
2025-09-25 14:46:51 +0530

    Newly Separated Admin Test Cases

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -2,8 +2,6 @@ import pytest
 import allure
 from pages.login_page import LoginPage
 from pages.dashboard_page import DashboardPage
-from pages.apply_leave_page import ApplyLeavePage
-
 
 @allure.epic("Dashboard Module")
 @allure.feature("Dashboard Functionality")
@@ -71,31 +69,3 @@ class TestDashboard:
 
         # Step 3: Click Leave List
         dashboard_page.click_leave_list()
-
-    @allure.epic("Leave Module")
-    @allure.feature("Leave Functionality")
-    @allure.story("Navigate to Apply Leave")
-    def test_apply_leave(self, driver):
-        login_page = LoginPage(driver)
-        dashboard_page = DashboardPage(driver)
-        apply_leave_page = ApplyLeavePage(driver)
-
-        # Step 1 : Login
-        login_page.open_login_page()
-        login_page.login("Admin", "admin123")
-        assert dashboard_page.is_dashboard_loaded()
-
-        # Step 2: Open Apply Leave form
-        dashboard_page.click_apply_leave()
-
-        # Step 3: Fill out the form
-        apply_leave_page.select_leave_type("CAN - Personal")
-        apply_leave_page.enter_from_date("2025-09-10")
-        apply_leave_page.enter_to_date("2025-09-12")
-        apply_leave_page.enter_comment("Vacation leave for family trip")
-
-        # Step 4 : Submit
-        apply_leave_page.submit()
-
-        # Step 5 : Validate success
-        assert apply_leave_page.is_leave_applied(), "❌ Leave was not applied successfully"
